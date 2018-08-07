65d8283e81a319123483939bac01ae6f9ff366c7
kibigo! <marrus-sh@users.noreply.github.com>
2018-08-06 22:57:18 -0700

    Added value space error

--- Sources/Common/Enums/Errors/NibError.swift
+++ Sources/Common/Enums/Errors/NibError.swift
@@ -4,4 +4,7 @@ public enum NibError: Error {
 	/// Only XML characters are allowed in `XSDRegularExpression`s.
 	case invalidCharacterInRegularExpression
 
+	/// The provided `representation` does not exist in the lexical space for the type.
+	case notInLexicalSpace
+
 }
