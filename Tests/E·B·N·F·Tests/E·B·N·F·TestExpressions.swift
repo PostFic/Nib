1ed13d4bbb0255914d37f756bea2e5cc1d93d314
kibigo! <marrus-sh@users.noreply.github.com>
2021-02-21 00:34:03 -0800

    Fixes to ranges and tests

--- Tests/E·B·N·F·Tests/E·B·N·F·TestExpressions.swift
+++ Tests/E·B·N·F·Tests/E·B·N·F·TestExpressions.swift
@@ -397,24 +397,39 @@ final class E·B·N·F·TestExpressions:
 		XCTAssertEqual(4 * Symbol.symbol′, [Symbol.symbol′, Symbol.symbol′, Symbol.symbol′, Symbol.symbol′])
 	}
 
-	func testExpressionMultiplesUpThrough () {
-		XCTAssertEqual(...0 * Symbol.symbol′, [])
-		XCTAssertEqual(...1 * Symbol.symbol′, Symbol.symbol°)
-		XCTAssertEqual(...4 * Symbol.symbol′, [Symbol.symbol′, [Symbol.symbol′, (Symbol.symbol′ & Symbol.symbol°)°]°]°)
-	}
-
 	func testExpressionMultiplesFrom () {
 		XCTAssertEqual(0... * Symbol.symbol′, Symbol.symbol*)
 		XCTAssertEqual(1... * Symbol.symbol′, Symbol.symbol″)
 		XCTAssertEqual(4... * Symbol.symbol′, [Symbol.symbol′, Symbol.symbol′, Symbol.symbol′, Symbol.symbol″])
 	}
 
-	func testExpressionMultiplesFromUpThrough () {
+	func testExpressionMultiplesThrough () {
+		XCTAssertEqual(...0 * Symbol.symbol′, [])
+		XCTAssertEqual(...1 * Symbol.symbol′, Symbol.symbol°)
+		XCTAssertEqual(...4 * Symbol.symbol′, [Symbol.symbol′, [Symbol.symbol′, (Symbol.symbol′ & Symbol.symbol°)°]°]°)
+	}
+
+	func testExpressionMultiplesUpTo () {
+		XCTAssertEqual(..<0 * Symbol.symbol′, [])
+		XCTAssertEqual(..<1 * Symbol.symbol′, [])
+		XCTAssertEqual(..<2 * Symbol.symbol′, Symbol.symbol°)
+		XCTAssertEqual(..<4 * Symbol.symbol′, [Symbol.symbol′, (Symbol.symbol′ & Symbol.symbol°)°]°)
+	}
+
+	func testExpressionMultiplesFromThrough () {
 		XCTAssertEqual((0...4) * Symbol.symbol′, [Symbol.symbol′, [Symbol.symbol′, (Symbol.symbol′ & Symbol.symbol°)°]°]°)
 		XCTAssertEqual((1...4) * Symbol.symbol′, [Symbol.symbol′, [Symbol.symbol′, (Symbol.symbol′ & Symbol.symbol°)°]°])
 		XCTAssertEqual((2...4) * Symbol.symbol′, [Symbol.symbol′, Symbol.symbol′, (Symbol.symbol′ & Symbol.symbol°)°])
 		XCTAssertEqual((3...4) * Symbol.symbol′, [Symbol.symbol′, Symbol.symbol′, Symbol.symbol′, Symbol.symbol°])
 		XCTAssertEqual((4...4) * Symbol.symbol′, [Symbol.symbol′, Symbol.symbol′, Symbol.symbol′, Symbol.symbol′])
+	}
+
+	func testExpressionMultiplesFromUpTo () {
+		XCTAssertEqual((0..<4) * Symbol.symbol′, [Symbol.symbol′, (Symbol.symbol′ & Symbol.symbol°)°]°)
+		XCTAssertEqual((1..<4) * Symbol.symbol′, [Symbol.symbol′, (Symbol.symbol′ & Symbol.symbol°)°])
+		XCTAssertEqual((2..<4) * Symbol.symbol′, [Symbol.symbol′, Symbol.symbol′, Symbol.symbol°])
+		XCTAssertEqual((3..<4) * Symbol.symbol′, [Symbol.symbol′, Symbol.symbol′, Symbol.symbol′])
+		XCTAssertEqual((4..<4) * Symbol.symbol′, [])
 	}
 
 }
